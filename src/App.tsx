5ce24eb8004652dc7e3ecbd6bd8c8fb048839640
Ibrahim Ibrahim <ibrahim@users-MacBook-Pro.local>
2022-08-21 22:39:51 -0700

    set up react typescript from scratch

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+import IMAGE from "./react.png";
+import BASKETBALL from "./basketball.svg";
+import { ClickCounter } from "./ClickCounter";
+
+export const App = () => {
+  return (
+    <>
+      <h1>
+        Hello React Typescript webpack starter template - {process.env.NODE_ENV}{" "}
+        - {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React logo" width="300" height="200" />
+      <img src={BASKETBALL} alt="React logo" width="300" height="200" />
+      <ClickCounter />
+    </>
+  );
+};
